0ad94e48bff9911dee81459cad122a1db26a5707
DDSRem <73049927+DDSRem@users.noreply.github.com>
2025-04-19 09:39:32 +0800

    Update __init__.py

--- plugins.v2/samediasyncdel/__init__.py
+++ plugins.v2/samediasyncdel/__init__.py
@@ -112,19 +112,6 @@ class SaMediaSyncDel(_PluginBase):
         self.save_data("history", historys)
         return schemas.Response(success=True, message="删除成功")
 
-    def get_service(self) -> List[Dict[str, Any]]:
-        """
-        注册插件公共服务
-        [{
-            "id": "服务ID",
-            "name": "服务名称",
-            "trigger": "触发器：cron/interval/date/CronTrigger.from_crontab()",
-            "func": self.xxx,
-            "kwargs": {} # 定时器参数
-        }]
-        """
-        pass
-
     def get_form(self) -> Tuple[List[dict], Dict[str, Any]]:
         """
         拼装插件配置页面，需要返回两块数据：1、页面配置；2、数据结构
