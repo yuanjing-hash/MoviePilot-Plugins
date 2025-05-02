fc647d7b5ab40e6713d55e862b92c213eae065d3
DDSRem <73049927+DDSRem@users.noreply.github.com>
2025-05-02 21:40:40 +0800

    fix

---
+++ plugins.v2/p123disk/__init__.py
@@ -0,0 +1,383 @@
+from pathlib import Path
+from typing import Any, List, Dict, Tuple, Optional
+
+from p123 import P123Client
+from .p123_api import P123Api
+
+from app import schemas
+from app.core.event import eventmanager, Event
+from app.log import logger
+from app.plugins import _PluginBase
+from app.schemas.types import ChainEventType
+from helper.storage import StorageHelper
+from schemas import StorageOperSelectionEventData, FileItem
+
+
+class P123Disk(_PluginBase):
+    # 插件名称
+    plugin_name = "123云盘储存"
+    # 插件描述
+    plugin_desc = "支持123云盘储存。"
+    # 插件图标
+    plugin_icon = "https://raw.githubusercontent.com/DDS-Derek/MoviePilot-Plugins/main/icons/P123Disk.png"
+    # 插件版本
+    plugin_version = "0.0.1"
+    # 插件作者
+    plugin_author = "DDSRem"
+    # 作者主页
+    author_url = "https://github.com/DDSRem"
+    # 插件配置项ID前缀
+    plugin_config_prefix = "p123disk_"
+    # 加载顺序
+    plugin_order = 99
+    # 可使用的用户级别
+    auth_level = 1
+
+    # 是否启用
+    _enabled = False
+    _client = None
+    _disk_name = None
+    _p123_api = None
+    _passport = None
+    _password = None
+
+    def init_plugin(self, config: dict = None):
+        self._disk_name = "123云盘"
+
+        if config:
+            storage_helper = StorageHelper()
+            storages = storage_helper.get_storagies()
+            print(list(storages))
+            if not any(
+                s.type == self._disk_name and s.name == self._disk_name
+                for s in storages
+            ):
+                # 添加云盘存储配置
+                storage_helper.add_storage(
+                    storage=self._disk_name, name=self._disk_name, conf={}
+                )
+                print(1)
+
+            self._enabled = config.get("enabled")
+            self._passport = config.get("passport")
+            self._password = config.get("password")
+
+            try:
+                self._client = P123Client(self._passport, self._password)
+                self._p123_api = P123Api(self._client)
+            except Exception as e:
+                logger.error(f"123云盘客户端创建失败: {e}")
+
+    def get_state(self) -> bool:
+        return self._enabled
+
+    @staticmethod
+    def get_command() -> List[Dict[str, Any]]:
+        pass
+
+    def get_api(self) -> List[Dict[str, Any]]:
+        pass
+
+    def get_form(self) -> Tuple[List[dict], Dict[str, Any]]:
+        """
+        拼装插件配置页面，需要返回两块数据：1、页面配置；2、数据结构
+        """
+        return [
+            {
+                "component": "VForm",
+                "content": [
+                    {
+                        "component": "VRow",
+                        "content": [
+                            {
+                                "component": "VCol",
+                                "props": {"cols": 12, "md": 4},
+                                "content": [
+                                    {
+                                        "component": "VSwitch",
+                                        "props": {
+                                            "model": "enabled",
+                                            "label": "启用插件",
+                                        },
+                                    }
+                                ],
+                            },
+                            {
+                                "component": "VCol",
+                                "props": {"cols": 12, "md": 4},
+                                "content": [
+                                    {
+                                        "component": "VTextField",
+                                        "props": {
+                                            "model": "passport",
+                                            "label": "手机号",
+                                        },
+                                    }
+                                ],
+                            },
+                            {
+                                "component": "VCol",
+                                "props": {"cols": 12, "md": 4},
+                                "content": [
+                                    {
+                                        "component": "VTextField",
+                                        "props": {
+                                            "model": "password",
+                                            "label": "密码",
+                                        },
+                                    }
+                                ],
+                            },
+                        ],
+                    },
+                ],
+            }
+        ], {
+            "enabled": False,
+            "passport": "",
+            "password": "",
+        }
+
+    def get_page(self) -> List[dict]:
+        pass
+
+    def get_module(self) -> Dict[str, Any]:
+        """
+        获取插件模块声明，用于胁持系统模块实现（方法名：方法实现）
+        {
+            "id1": self.xxx1,
+            "id2": self.xxx2,
+        }
+        """
+        return {
+            "list_files": self.list_files,
+            "any_files": self.any_files,
+            "download_file": self.download_file,
+            "upload_file": self.upload_file,
+            "delete_file": self.delete_file,
+            "rename_file": self.rename_file,
+            "get_file_item": self.get_file_item,
+            "get_parent_item": self.get_parent_item,
+            "snapshot_storage": self.snapshot_storage,
+            "storage_usage": self.storage_usage,
+            "support_transtype": self.support_transtype,
+            "create_folder": self.create_folder,
+            "exists": self.exists,
+            "get_item": self.get_item,
+        }
+
+    @eventmanager.register(ChainEventType.StorageOperSelection)
+    def storage_oper_selection(self, event: Event):
+        """
+        监听存储选择事件，返回当前类为操作对象
+        """
+        if not self._enabled:
+            return
+        event_data: StorageOperSelectionEventData = event.event_data
+        if event_data.storage == self._disk_name:
+            # 处理云盘的操作
+            event_data.storage_oper = self._p123_api
+
+    def list_files(
+        self, fileitem: schemas.FileItem, recursion: bool = False
+    ) -> Optional[List[schemas.FileItem]]:
+        """
+        查询当前目录下所有目录和文件
+        """
+
+        if fileitem.storage != self._disk_name:
+            return None
+
+        def __get_files(_item: FileItem, _r: Optional[bool] = False):
+            """
+            递归处理
+            """
+            _items = self._p123_api.list(_item)
+            if _items:
+                if _r:
+                    for t in _items:
+                        if t.type == "dir":
+                            __get_files(t, _r)
+                        else:
+                            result.append(t)
+                else:
+                    result.extend(_items)
+
+        # 返回结果
+        result = []
+        __get_files(fileitem, recursion)
+
+        return result
+
+    def any_files(
+        self, fileitem: schemas.FileItem, extensions: list = None
+    ) -> Optional[bool]:
+        """
+        查询当前目录下是否存在指定扩展名任意文件
+        """
+        if fileitem.storage != self._disk_name:
+            return None
+
+        def __any_file(_item: FileItem):
+            """
+            递归处理
+            """
+            _items = self._p123_api.list(_item)
+            if _items:
+                if not extensions:
+                    return True
+                for t in _items:
+                    if (
+                        t.type == "file"
+                        and t.extension
+                        and f".{t.extension.lower()}" in extensions
+                    ):
+                        return True
+                    elif t.type == "dir":
+                        if __any_file(t):
+                            return True
+            return False
+
+        # 返回结果
+        return __any_file(fileitem)
+
+    def create_folder(
+        self, fileitem: schemas.FileItem, name: str
+    ) -> Optional[schemas.FileItem]:
+        """
+        创建目录
+        """
+        if fileitem.storage != self._disk_name:
+            return None
+
+        return self._p123_api.create_folder(fileitem=fileitem, name=name)
+
+    def download_file(
+        self, fileitem: schemas.FileItem, path: Path = None
+    ) -> Optional[Path]:
+        """
+        下载文件
+        :param fileitem: 文件项
+        :param path: 本地保存路径
+        """
+        if fileitem.storage != self._disk_name:
+            return None
+
+        return self._p123_api.download(fileitem, path)
+
+    def upload_file(
+        self, fileitem: schemas.FileItem, path: Path, new_name: Optional[str] = None
+    ) -> Optional[schemas.FileItem]:
+        """
+        上传文件
+        :param fileitem: 保存目录项
+        :param path: 本地文件路径
+        :param new_name: 新文件名
+        """
+        if fileitem.storage != self._disk_name:
+            return None
+
+        return self._p123_api.upload(fileitem, path, new_name)
+
+    def delete_file(self, fileitem: schemas.FileItem) -> Optional[bool]:
+        """
+        删除文件或目录
+        """
+        if fileitem.storage != self._disk_name:
+            return None
+
+        return self._p123_api.delete(fileitem)
+
+    def rename_file(self, fileitem: schemas.FileItem, name: str) -> Optional[bool]:
+        """
+        重命名文件或目录
+        """
+        if fileitem.storage != self._disk_name:
+            return None
+
+        return self._p123_api.rename(fileitem, name)
+
+    def exists(self, fileitem: schemas.FileItem) -> Optional[bool]:
+        """
+        判断文件或目录是否存在
+        """
+        if fileitem.storage != self._disk_name:
+            return None
+
+        return True if self.get_item(fileitem) else False
+
+    def get_item(self, fileitem: schemas.FileItem) -> Optional[schemas.FileItem]:
+        """
+        查询目录或文件
+        """
+        if fileitem.storage != self._disk_name:
+            return None
+
+        return self.get_file_item(storage=fileitem.storage, path=Path(fileitem.path))
+
+    def get_file_item(self, storage: str, path: Path) -> Optional[schemas.FileItem]:
+        """
+        根据路径获取文件项
+        """
+        if storage != self._disk_name:
+            return None
+
+        return self._p123_api.get_item(path)
+
+    def get_parent_item(self, fileitem: schemas.FileItem) -> Optional[schemas.FileItem]:
+        """
+        获取上级目录项
+        """
+        if fileitem.storage != self._disk_name:
+            return None
+
+        return self._p123_api.get_parent(fileitem)
+
+    def snapshot_storage(self, storage: str, path: Path) -> Optional[Dict[str, float]]:
+        """
+        快照存储
+        """
+        if storage != self._disk_name:
+            return None
+
+        files_info = {}
+
+        def __snapshot_file(_fileitm: schemas.FileItem):
+            """
+            递归获取文件信息
+            """
+            if _fileitm.type == "dir":
+                for sub_file in self._p123_api.list(_fileitm):
+                    __snapshot_file(sub_file)
+            else:
+                files_info[_fileitm.path] = _fileitm.size
+
+        fileitem = self._p123_api.get_item(path)
+        if not fileitem:
+            return {}
+
+        __snapshot_file(fileitem)
+
+        return files_info
+
+    def storage_usage(self, storage: str) -> Optional[schemas.StorageUsage]:
+        """
+        存储使用情况
+        """
+        if storage != self._disk_name:
+            return None
+
+        return self._p123_api.usage()
+
+    @staticmethod
+    def support_transtype(storage: str) -> Optional[dict]:
+        """
+        获取支持的整理方式
+        """
+        return {"move": "移动", "copy": "复制"}
+
+    def stop_service(self):
+        """
+        退出插件
+        """
+        pass
---
+++ plugins.v2/p123disk/p123_api.py
@@ -0,0 +1,432 @@
+import json
+from pathlib import Path
+from typing import Optional, List, Dict
+from datetime import datetime
+
+import pytz
+import requests
+from p123 import P123Client, check_response
+
+import schemas
+from app.core.config import settings
+from app.log import logger
+
+
+class P123Api:
+    """
+    123云盘基础操作
+    """
+
+    _disk_name = "123云盘"
+
+    # FileId和路径缓存
+    _id_cache: Dict[str, str] = {}
+
+    def __init__(self, client: P123Client):
+        self.client = client
+
+    def _path_to_id(self, path: str):
+        """
+        通过路径获取ID
+        """
+        # 根目录
+        if path == "/":
+            return "0"
+        if len(path) > 1 and path.endswith("/"):
+            path = path[:-1]
+        # 检查缓存
+        if path in self._id_cache:
+            return self._id_cache[path]
+        # 逐级查找缓存
+        current_id = 0
+        parent_path = "/"
+        for p in Path(path).parents:
+            if str(p) in self._id_cache:
+                parent_path = str(p)
+                current_id = self._id_cache[parent_path]
+                break
+        # 计算相对路径
+        rel_path = Path(path).relative_to(parent_path)
+        for part in Path(rel_path).parts:
+            find_part = False
+            payload = {
+                "parentFileId": int(current_id),
+            }
+            resp = self.client.fs_list(payload)
+            check_response(resp)
+            for item in resp["data"]["InfoList"]:
+                if item["FileName"] == part:
+                    current_id = item["FileId"]
+                    find_part = True
+                    break
+            if not find_part:
+                raise FileNotFoundError(f"【123】{path} 不存在")
+        if not current_id:
+            raise FileNotFoundError(f"【123】{path} 不存在")
+        # 缓存路径
+        self._id_cache[path] = str(current_id)
+        return str(current_id)
+
+    def list(self, fileitem: schemas.FileItem) -> List[schemas.FileItem]:
+        """
+        浏览文件
+        """
+        if fileitem.type == "file":
+            item = self.detail(fileitem)
+            if item:
+                return [item]
+            return []
+        if fileitem.path == "/":
+            file_id = "0"
+        else:
+            file_id = fileitem.fileid
+            if not file_id:
+                file_id = self._path_to_id(fileitem.path)
+
+        items = []
+        try:
+            payload = {
+                "parentFileId": int(file_id),
+            }
+            resp = self.client.fs_list(payload)
+            check_response(resp)
+        except Exception as e:
+            logger.debug(f"【123】获取信息失败: {str(e)}")
+            return items
+        for item in resp["data"]["InfoList"]:
+            path = f"{fileitem.path}{item['FileName']}"
+            self._id_cache[path] = str(item["FileId"])
+
+            file_path = path + ("/" if item["Type"] == 1 else "")
+            items.append(
+                schemas.FileItem(
+                    storage=self._disk_name,
+                    fileid=str(item["FileId"]),
+                    parent_fileid=str(item["ParentFileId"]),
+                    name=item["FileName"],
+                    basename=Path(item["FileName"]).stem,
+                    extension=Path(item["FileName"]).suffix[1:]
+                    if item["Type"] == 0
+                    else None,
+                    type="dir" if item["Type"] == 1 else "file",
+                    path=file_path,
+                    size=item["Size"] if item["Type"] == 0 else None,
+                    modify_time=int(
+                        datetime.fromisoformat(item["UpdateAt"]).timestamp()
+                    ),
+                    pickcode=str(item),
+                )
+            )
+        return items
+
+    def create_folder(
+        self, fileitem: schemas.FileItem, name: str
+    ) -> Optional[schemas.FileItem]:
+        """
+        创建目录
+        :param fileitem: 父目录
+        :param name: 目录名
+        """
+        try:
+            new_path = Path(fileitem.path) / name
+            resp = self.client.fs_mkdir(name, parent_id=self._path_to_id(fileitem.path))
+            check_response(resp)
+            data = resp["data"]["Info"]
+            # 缓存新目录
+            self._id_cache[str(new_path)] = str(data["FileId"])
+            return schemas.FileItem(
+                storage=self._disk_name,
+                fileid=str(data["FileId"]),
+                path=str(new_path) + "/",
+                name=name,
+                basename=name,
+                type="dir",
+                modify_time=int(datetime.fromisoformat(data["UpdateAt"]).timestamp()),
+                pickcode=str(data),
+            )
+        except Exception as e:
+            logger.debug(f"【123】创建目录失败: {str(e)}")
+            return None
+
+    def get_folder(self, path: Path) -> Optional[schemas.FileItem]:
+        """
+        获取目录，如目录不存在则创建
+        """
+
+        def __find_dir(
+            _fileitem: schemas.FileItem, _name: str
+        ) -> Optional[schemas.FileItem]:
+            """
+            查找下级目录中匹配名称的目录
+            """
+            for sub_folder in self.list(_fileitem):
+                if sub_folder.type != "dir":
+                    continue
+                if sub_folder.name == _name:
+                    return sub_folder
+            return None
+
+        # 是否已存在
+        folder = self.get_item(path)
+        if folder:
+            return folder
+        # 逐级查找和创建目录
+        fileitem = schemas.FileItem(storage=self._disk_name, path="/")
+        for part in path.parts[1:]:
+            dir_file = __find_dir(fileitem, part)
+            if dir_file:
+                fileitem = dir_file
+            else:
+                dir_file = self.create_folder(fileitem, part)
+                if not dir_file:
+                    logger.warn(f"【123】创建目录 {fileitem.path}{part} 失败！")
+                    return None
+                fileitem = dir_file
+        return fileitem
+
+    def get_item(self, path: Path) -> Optional[schemas.FileItem]:
+        """
+        获取文件或目录，不存在返回None
+        """
+        try:
+            file_id = self._path_to_id(str(path))
+            if not file_id:
+                return None
+            resp = self.client.fs_info(int(file_id))
+            check_response(resp)
+            data = resp["data"]["infoList"][0]
+            return schemas.FileItem(
+                storage=self._disk_name,
+                fileid=str(data["FileId"]),
+                path=str(path) + ("/" if data["Type"] == 1 else ""),
+                type="file" if data["Type"] == 0 else "dir",
+                name=data["FileName"],
+                basename=Path(data["FileName"]).stem,
+                extension=Path(data["FileName"]).suffix[1:]
+                if data["Type"] == 0
+                else None,
+                pickcode=str(data),
+                size=data["Size"] if data["Type"] == 0 else None,
+                modify_time=int(datetime.fromisoformat(data["UpdateAt"]).timestamp()),
+            )
+        except Exception as e:
+            logger.debug(f"【123】获取文件信息失败: {str(e)}")
+            return None
+
+    def get_parent(self, fileitem: schemas.FileItem) -> Optional[schemas.FileItem]:
+        """
+        获取父目录
+        """
+        return self.get_item(Path(fileitem.path).parent)
+
+    def delete(self, fileitem: schemas.FileItem) -> bool:
+        """
+        删除文件
+        """
+        try:
+            resp = self.client.fs_delete(int(fileitem.fileid))
+            check_response(resp)
+            return True
+        except Exception:
+            return False
+
+    def rename(self, fileitem: schemas.FileItem, name: str) -> bool:
+        """
+        重命名文件
+        """
+        try:
+            payload = {
+                "FileId": int(fileitem.fileid),
+                "fileName": name,
+                "duplicate": 2,
+            }
+            resp = self.client.fs_rename(payload)
+            check_response(resp)
+            return True
+        except Exception:
+            return False
+
+    def download(self, fileitem: schemas.FileItem, path: Path = None) -> Path:
+        """
+        下载文件，保存到本地，返回本地临时文件地址
+        :param fileitem: 文件项
+        :param path: 文件保存路径
+        """
+        json_obj = json.loads(fileitem.pickcode)
+        s3keyflag = json_obj["S3KeyFlag"]
+        file_id = fileitem.fileid
+        file_name = fileitem.name
+        md5 = json_obj["Etag"]
+        size = json_obj["Size"]
+        try:
+            payload = {
+                "Etag": md5,
+                "FileID": int(file_id),
+                "FileName": file_name,
+                "S3KeyFlag": s3keyflag,
+                "Size": int(size),
+            }
+            resp = self.client.download_info(payload)
+            check_response(resp)
+            download_url = resp["data"]["DownloadUrl"]
+            local_path = path or settings.TEMP_PATH / fileitem.name
+            with requests.get(download_url, stream=True) as r:
+                r.raise_for_status()
+                with open(local_path, "wb") as f:
+                    for chunk in r.iter_content(chunk_size=8192):
+                        f.write(chunk)
+            return local_path
+        except Exception:
+            return None
+
+    def upload(
+        self,
+        target_dir: schemas.FileItem,
+        local_path: Path,
+        new_name: Optional[str] = None,
+    ) -> Optional[schemas.FileItem]:
+        """
+        上传文件
+        :param target_dir: 上传目录项
+        :param local_path: 本地文件路径
+        :param new_name: 上传后文件名
+        """
+        target_name = new_name or local_path.name
+        target_path = Path(target_dir.path) / target_name
+        try:
+            resp = self.client.upload_file_fast(
+                file=local_path,
+                duplicate=2,
+                file_name=new_name,
+                parent_id=target_dir.fileid,
+            )
+            check_response(resp)
+            if resp["data"]["Info"]:
+                data = resp["data"]["Info"]
+                return schemas.FileItem(
+                    storage=self._disk_name,
+                    fileid=str(data["FileId"]),
+                    path=str(target_path) + ("/" if data["Type"] == 1 else ""),
+                    type="file" if data["Type"] == 0 else "dir",
+                    name=data["FileName"],
+                    basename=Path(data["FileName"]).stem,
+                    extension=Path(data["FileName"]).suffix[1:]
+                    if data["Type"] == 0
+                    else None,
+                    pickcode=str(data),
+                    size=data["Size"] if data["Type"] == 0 else None,
+                    modify_time=int(
+                        datetime.fromisoformat(data["UpdateAt"]).timestamp()
+                    ),
+                )
+
+            resp = self.client.upload_file(
+                file=local_path,
+                duplicate=2,
+                file_name=new_name,
+                parent_id=target_dir.fileid,
+            )
+            check_response(resp)
+            if resp["data"]["Info"]:
+                data = resp["data"]["Info"]
+                return schemas.FileItem(
+                    storage=self._disk_name,
+                    fileid=str(data["FileId"]),
+                    path=str(target_path) + ("/" if data["Type"] == 1 else ""),
+                    type="file" if data["Type"] == 0 else "dir",
+                    name=data["FileName"],
+                    basename=Path(data["FileName"]).stem,
+                    extension=Path(data["FileName"]).suffix[1:]
+                    if data["Type"] == 0
+                    else None,
+                    pickcode=str(data),
+                    size=data["Size"] if data["Type"] == 0 else None,
+                    modify_time=int(
+                        datetime.fromisoformat(data["UpdateAt"]).timestamp()
+                    ),
+                )
+
+            logger.error("【123】上传文件失败")
+            return None
+        except Exception as e:
+            logger.error(f"【123】上传文件失败: {e}")
+            return None
+
+    def detail(self, fileitem: schemas.FileItem) -> Optional[schemas.FileItem]:
+        """
+        获取文件详情
+        """
+        return self.get_item(Path(fileitem.path))
+
+    def copy(self, fileitem: schemas.FileItem, path: Path, new_name: str) -> bool:
+        """
+        复制文件
+        :param fileitem: 文件项
+        :param path: 目标目录
+        :param new_name: 新文件名
+        """
+        try:
+            resp = self.client.client.fs_copy(
+                fileitem.fileid, parent_id=self._path_to_id(str(path))
+            )
+            check_response(resp)
+            new_path = Path(path) / fileitem.name
+            new_item = self.get_item(new_path)
+            self.rename(new_item, new_name)
+            # 更新缓存
+            del self._id_cache[fileitem.path]
+            rename_new_path = Path(path) / new_name
+            self._id_cache[str(rename_new_path)] = new_item.fileid
+            return True
+        except Exception:
+            return False
+
+    def move(self, fileitem: schemas.FileItem, path: Path, new_name: str) -> bool:
+        """
+        移动文件
+        :param fileitem: 文件项
+        :param path: 目标目录
+        :param new_name: 新文件名
+        """
+        try:
+            resp = self.client.client.fs_move(
+                fileitem.fileid, parent_id=self._path_to_id(str(path))
+            )
+            check_response(resp)
+            new_path = Path(path) / fileitem.name
+            new_item = self.get_item(new_path)
+            self.rename(new_item, new_name)
+            # 更新缓存
+            del self._id_cache[fileitem.path]
+            rename_new_path = Path(path) / new_name
+            self._id_cache[str(rename_new_path)] = new_item.fileid
+            return True
+        except Exception:
+            return False
+
+    def link(self, fileitem: schemas.FileItem, target_file: Path) -> bool:
+        """
+        硬链接文件
+        """
+        pass
+
+    def softlink(self, fileitem: schemas.FileItem, target_file: Path) -> bool:
+        """
+        软链接文件
+        """
+        pass
+
+    def usage(self) -> Optional[schemas.StorageUsage]:
+        """
+        存储使用情况
+        """
+        try:
+            resp = self.client.user_info()
+            check_response(resp)
+            return schemas.StorageUsage(
+                total=resp["data"]["SpacePermanent"],
+                available=int(resp["data"]["SpacePermanent"])
+                - int(resp["data"]["SpaceUsed"]),
+            )
+        except Exception:
+            return None
