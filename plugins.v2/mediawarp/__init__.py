3abe9cdaaf12b23052b466c2623a415f9ebfd8ed
DDSRem <73049927+DDSRem@users.noreply.github.com>
2025-05-16 18:33:51 +0800

    fix

--- plugins.v2/mediawarp/__init__.py
+++ plugins.v2/mediawarp/__init__.py
@@ -28,7 +28,7 @@ class MediaWarp(_PluginBase):
     # 插件图标
     plugin_icon = "https://raw.githubusercontent.com/jxxghp/MoviePilot-Plugins/refs/heads/main/icons/cloud.png"
     # 插件版本
-    plugin_version = "1.0.1"
+    plugin_version = "1.0.2"
     # 插件作者
     plugin_author = "DDSRem"
     # 作者主页
