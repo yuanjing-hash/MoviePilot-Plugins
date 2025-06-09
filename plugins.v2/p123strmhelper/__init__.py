1a4bd78ba44eede32c6d9d7851f294256f0a25c6
DDSRem <73049927+DDSRem@users.noreply.github.com>
2025-06-09 14:46:55 +0800

    fix

--- plugins.v2/p123strmhelper/__init__.py
+++ plugins.v2/p123strmhelper/__init__.py
@@ -299,9 +299,15 @@ class ShareStrmHelper:
         self.client = client
         self.strm_count = 0
         self.mediainfo_count = 0
+        self.strm_fail_count = 0
+        self.mediainfo_fail_count = 0
+        self.strm_fail_dict: Dict[str, str] = {}
+        self.mediainfo_fail_dict: List = None
         self.share_media_path = share_media_path
         self.local_media_path = local_media_path
         self.server_address = server_address.rstrip("/")
+        self._mediainfodownloader = MediaInfoDownloader(client=self.client)
+        self.download_mediainfo_list = []
 
     def has_prefix(self, full_path, prefix_path):
         """
@@ -342,76 +348,71 @@ class ShareStrmHelper:
                 self.share_media_path
             )
             file_target_dir = file_path.parent
-            original_file_name = file_path.name
             file_name = file_path.stem + ".strm"
             new_file_path = file_target_dir / file_name
 
-            if self.auto_download_mediainfo:
-                if file_path.suffix in self.download_mediaext:
-                    payload = {
-                        "Etag": item["Etag"],
-                        "FileID": int(item["FileId"]),
-                        "FileName": item["FileName"],
-                        "S3KeyFlag": item["S3KeyFlag"],
-                        "Size": int(item["Size"]),
-                    }
-                    resp = self.client.download_info(
-                        payload,
-                        base_url="",
-                        async_=False,
-                        headers={"User-Agent": settings.USER_AGENT},
-                    )
-                    check_response(resp)
-                    download_url = resp["data"]["DownloadUrl"]
-
-                    if not download_url:
-                        logger.error(
-                            f"【分享STRM生成】{original_file_name} 下载链接获取失败，无法下载该文件"
+            try:
+                if self.auto_download_mediainfo:
+                    if file_path.suffix in self.download_mediaext:
+                        self.download_mediainfo_list.append(
+                            [
+                                {
+                                    "Etag": item["Etag"],
+                                    "FileID": int(item["FileId"]),
+                                    "FileName": item["FileName"],
+                                    "S3KeyFlag": item["S3KeyFlag"],
+                                    "Size": int(item["Size"]),
+                                },
+                                str(file_path),
+                            ]
                         )
                         continue
 
-                    file_path.parent.mkdir(parents=True, exist_ok=True)
-
-                    with requests.get(
-                        download_url,
-                        stream=True,
-                        timeout=30,
-                        headers={
-                            "User-Agent": settings.USER_AGENT,
-                        },
-                    ) as response:
-                        response.raise_for_status()
-                        with open(file_path, "wb") as f:
-                            for chunk in response.iter_content(chunk_size=8192):
-                                f.write(chunk)
-
-                    logger.info(
-                        f"【分享STRM生成】保存 {original_file_name} 文件成功: {file_path}"
+                if file_path.suffix not in self.rmt_mediaext:
+                    logger.warn(
+                        "【分享STRM生成】文件后缀不匹配，跳过网盘路径: %s",
+                        str(file_path).replace(str(self.local_media_path), "", 1),
                     )
-                    self.mediainfo_count += 1
-                    logger.info("【分享STRM生成】休眠 1s 后继续生成")
-                    time.sleep(1)
                     continue
 
-            if file_path.suffix not in self.rmt_mediaext:
-                logger.warn(
-                    "【分享STRM生成】文件后缀不匹配，跳过网盘路径: %s",
-                    str(file_path).replace(str(self.local_media_path), "", 1),
+                new_file_path.parent.mkdir(parents=True, exist_ok=True)
+
+                strm_url = f"{self.server_address}/api/v1/plugin/P123StrmHelper/redirect_url?apikey={settings.API_TOKEN}&name={item['FileName']}&size={item['Size']}&md5={item['Etag']}&s3_key_flag={item['S3KeyFlag']}"
+
+                with open(new_file_path, "w", encoding="utf-8") as file:
+                    file.write(strm_url)
+                self.strm_count += 1
+                logger.info(
+                    "【分享STRM生成】生成 STRM 文件成功: %s", str(new_file_path)
                 )
+            except Exception as e:
+                logger.error(
+                    "【分享STRM生成】生成 STRM 文件失败: %s  %s",
+                    str(new_file_path),
+                    e,
+                )
+                self.strm_fail_count += 1
+                self.strm_fail_dict[str(new_file_path)] = str(e)
                 continue
 
-            new_file_path.parent.mkdir(parents=True, exist_ok=True)
-
-            strm_url = f"{self.server_address}/api/v1/plugin/P123StrmHelper/redirect_url?apikey={settings.API_TOKEN}&name={item['FileName']}&size={item['Size']}&md5={item['Etag']}&s3_key_flag={item['S3KeyFlag']}"
-
-            with open(new_file_path, "w", encoding="utf-8") as file:
-                file.write(strm_url)
-            self.strm_count += 1
-            logger.info("【分享STRM生成】生成 STRM 文件成功: %s", str(new_file_path))
-
+        self.mediainfo_count, self.mediainfo_fail_count, self.mediainfo_fail_dict = (
+            self._mediainfodownloader.auto_downloader(
+                downloads_list=self.download_mediainfo_list
+            )
+        )
+        if self.strm_fail_dict:
+            for path, error in self.strm_fail_dict.items():
+                logger.warn(f"【分享STRM生成】{path} 生成错误原因: {error}")
+        if self.mediainfo_fail_dict:
+            for path in self.mediainfo_fail_dict:
+                logger.warn(f"【分享STRM生成】{path} 下载错误")
         logger.info(
             f"【分享STRM生成】分享生成 STRM 文件完成，总共生成 {self.strm_count} 个 STRM 文件，下载 {self.mediainfo_count} 个媒体数据文件"
         )
+        if self.strm_fail_count != 0 or self.mediainfo_fail_count != 0:
+            logger.warn(
+                f"【分享STRM生成】{self.strm_fail_count} 个 STRM 文件生成失败，{self.mediainfo_fail_count} 个媒体数据文件下载失败"
+            )
 
 
 class P123StrmHelper(_PluginBase):
@@ -1731,7 +1732,7 @@ class P123StrmHelper(_PluginBase):
         ):
             return
 
-        if not self._user_share_code or not self._user_share_pwd:
+        if not self._user_share_code:
             return
         share_code = self._user_share_code
         share_pwd = self._user_share_pwd
