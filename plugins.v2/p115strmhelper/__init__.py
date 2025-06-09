0dc589e9c25ef35e2071b37c8cf18554ce1e618d
DDSRem <73049927+DDSRem@users.noreply.github.com>
2025-06-09 17:36:22 +0800

    Revert "fix"
    This reverts commit 4e9b0670204e6cd29f6acaba8a06c1a5ae009ee1.

--- plugins.v2/p115strmhelper/__init__.py
+++ plugins.v2/p115strmhelper/__init__.py
@@ -624,10 +624,7 @@ class IncrementSyncStrmHelper:
         """
         cid = self.id_path_cache.get_id_by_dir(path)
         if not cid:
-            try:
-                data = self.databasehelper.get_by_path(path=path)
-            except MultipleResultsFound:
-                self.databasehelper.remove_by_path_batch(path=path)
+            data = self.databasehelper.get_by_path(path=path)
             if data:
                 cid = data.get("id", None)
                 if cid:
