72d51091d47ec32aec1049e751b7cfa32e660c67
DDSRem <73049927+DDSRem@users.noreply.github.com>
2025-06-28 09:41:01 +0800

    fix

--- plugins.v2/p123disk/p123_api.py
+++ plugins.v2/p123disk/p123_api.py
@@ -12,8 +12,6 @@ import schemas
 from app.core.config import settings
 from app.log import logger
 
-from .upload import upload_file
-
 
 class P123Api:
     """
@@ -374,8 +372,7 @@ class P123Api:
                             ),
                         )
 
-                    resp = upload_file(
-                        self.client,
+                    resp = self.client.upload_file(
                         file=local_path,
                         duplicate=2,
                         file_name=new_name,
