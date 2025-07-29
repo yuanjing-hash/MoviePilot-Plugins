66a4b08c48c77c81405582f653c9478acd589afa
DDSRem <73049927+DDSRem@users.noreply.github.com>
2025-07-29 21:13:32 +0800

    update

--- plugins.v2/p115strmhelper/__init__.py
+++ plugins.v2/p115strmhelper/__init__.py
@@ -45,7 +45,7 @@ class P115StrmHelper(_PluginBase):
     # 插件图标
     plugin_icon = "https://raw.githubusercontent.com/jxxghp/MoviePilot-Frontend/refs/heads/v2/src/assets/images/misc/u115.png"
     # 插件版本
-    plugin_version = "1.9.10"
+    plugin_version = "1.9.11"
     # 插件作者
     plugin_author = "DDSRem"
     # 作者主页
