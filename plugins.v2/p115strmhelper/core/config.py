ed94579fb215ce37432c0a0093db3d4341ae3f34
DDSRem <73049927+DDSRem@users.noreply.github.com>
2025-08-12 12:40:03 +0800

    update

--- plugins.v2/p115strmhelper/core/config.py
+++ plugins.v2/p115strmhelper/core/config.py
@@ -364,7 +364,7 @@ class ConfigManager(BaseModel):
         if utype in user_agents:
             return user_agents[utype]
         return (
-            f"{self.PLUSIN_NAME}/2.0.20 "
+            f"{self.PLUSIN_NAME}/2.0.23 "
             f"({platform.system()} {platform.release()}; "
             f"{SystemUtils.cpu_arch() if hasattr(SystemUtils, 'cpu_arch') and callable(SystemUtils.cpu_arch) else 'UnknownArch'})"
         )
